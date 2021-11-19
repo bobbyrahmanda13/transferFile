19fffd2d3710cba4caf97e1c83f1dd9fcb73c58e
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2021-11-19 11:16:55 -0500

    edited server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 const app = express();
-// app.use(express.static("public"));
+app.use(express.static("views"));
 
 
 // parse application/json

aaf4d4ba4fd4347cda00aa272efdd84653e476af
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2024-05-05 13:41:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 npm install --save express multer serve-index ejs ip body-parser nodemon
 cd ..
-#termux-fix-shebang $PREFIX/bin/nodemon
+npm install -g nodemon
+termux-fix-shebang $PREFIX/bin/nodemon
 cd transferFile
 mv bagiFile.sh ../.shortcuts
 chmod +x ../.shortcuts/bagiFile.sh
